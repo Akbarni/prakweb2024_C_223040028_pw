6053e867c1e1549b20c0cfb34538cf762adf1883
Akbar Nur iman <122083444+Akbarni@users.noreply.github.com>
2024-10-28 22:45:48 +0700

    mengerjakan mvc

---
+++ latihan mvc/mvc/sesi11/app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define("BASEURL", "http://localhost/prakweb_2024_C_223040028/latihan%20mvc/mvc/sesi11/public/");
+
+// DATANASE
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'php_mvc');
